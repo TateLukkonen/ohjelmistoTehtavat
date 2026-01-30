15d4b342ba48b5ff8b48f2418cc854683c84b28f
TateLukkonen <s2401191@edu.bc.fi>
2026-01-30 11:50:09 +0200

    add

--- react/tehtava1/src/App.jsx
+++ react/tehtava1/src/App.jsx
@@ -1,4 +1,12 @@
 import * as React from 'react'
+import Tervehdys from "./tervehdys";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <Tervehdys nimi="Tate" />
+    </div>
+  );
+}
+
 export default App
