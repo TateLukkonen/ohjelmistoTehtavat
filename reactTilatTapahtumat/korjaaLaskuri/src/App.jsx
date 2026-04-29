cfa34d90d69dde352348ce4ab85a8d3b8afb0748
TateLukkonen <s2401191@edu.bc.fi>
2026-04-29 14:40:00 +0300

    add korjaa laskuri

---
+++ reactTilatTapahtumat/korjaaLaskuri/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+function Laskuri() {
+
+  const [count, setCount] = useState(0);
+
+ 
+  function increase() {
+    setCount(count + 1);
+  }
+
+ 
+  function reset() {
+    setCount(count - count);
+  }
+
+ 
+  return (
+
+    <div>
+
+      <p>Count: {count}</p>
+
+      <button onClick={increase}>Kasvata</button>
+
+      <button onClick={reset}>Nollaa</button>
+
+    </div>
+
+  );
+
+}
+
+ 
+export default Laskuri;
